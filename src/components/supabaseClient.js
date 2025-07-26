dce56bb3cf4920f935c82f3c8876503296d59e02
VixenHavoc1 <vixenhavoc1@gmail.com>
2025-07-26 19:43:59 +0530

    Update supabaseClient.js

--- src/components/supabaseClient.js
+++ src/components/supabaseClient.js
@@ -3,7 +3,7 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Replace these with your actual Supabase project URL and anon key
-const SUPABASE_URL = 'https://rehcxrsbpawciqsfgiop.supabase.co'
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlaGN4cnNicGF3Y2lxc2ZnaW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczOTY5ODcsImV4cCI6MjA2Mjk3Mjk4N30.1nWEbTbF45Z8NJibMouVpLh4s55UCZMzh82wa5eBEy0'
+const NEXT_PUBLIC_SUPABASE_URL = 'https://rehcxrsbpawciqsfgiop.supabase.co'
+const NEXT_PUBLIC_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJlaGN4cnNicGF3Y2lxc2ZnaW9wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDczOTY5ODcsImV4cCI6MjA2Mjk3Mjk4N30.1nWEbTbF45Z8NJibMouVpLh4s55UCZMzh82wa5eBEy0'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
