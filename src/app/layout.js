0e944f273683531e5555ac2924815423397c0d6d
VixenHavoc1 <vixenhavoc1@gmail.com>
2025-11-11 19:09:09 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,8 +7,8 @@ const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
 
 export const metadata = {
-  title: "Voxella AI",
-  description: "AI Girlfriend",
+  title: "Voxella",
+  description: "Mind Companion",
   viewport: "width=device-width, initial-scale=1.0", // ✅ This makes it mobile-responsive
 };
 
